ace7112a5ffdaf4dd48e0da256ca34053f28a6f6
mansimaurya19 <mansimaurya0@gmail.com>
2020-09-12 02:43:06 +0530

    Transaction cards

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,15 +1,25 @@
 import React, { useContext, useEffect } from 'react';
 import AuthContext from '../../context/auth/authContext';
 import Dashboard from '../sections/Dashboard';
+import Transactions from '../sections/Transactions';
 
 const Home = () => {
-  const authContext = useContext(AuthContext);
-  useEffect(() => {
-    authContext.loadUser();
-    // eslint-disable-next-line
-  }, []);
-  const { isAuthenticated, logout, user } = authContext;
-  return <div>{user ? <Dashboard /> : 'Home'}</div>;
+	const authContext = useContext(AuthContext);
+	useEffect(() => {
+		authContext.loadUser();
+		// eslint-disable-next-line
+	}, []);
+	const { isAuthenticated, logout, user } = authContext;
+	if (user) {
+		return (
+			<div>
+				<Dashboard />
+				<Transactions />
+			</div>
+		);
+	} else {
+		return <div>Loading..</div>;
+	}
 };
 
 export default Home;
