d687f12aa4d432a8837dbeb716963aa3818f92ff
Aditya Jee <jeeadityajha1@gmail.com>
2020-09-11 11:34:52 +0530

    Updated Navbar & UI

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,45 +4,49 @@ import { Link } from 'react-router-dom';
 import AuthContext from '../../context/auth/authContext';
 
 const Navbar = ({ title, icon }) => {
-	const authContext = useContext(AuthContext);
-	const { isAuthenticated, logout, user } = authContext;
-	console.log(user);
-	const onLogout = () => {
-		logout();
-	};
-	const authLinks = (
-		<Fragment>
-			<li>Hello {user && user.name} </li>
-			<a onClick={onLogout} href='#!'>
-				<i className='fas fa-sign-out-alt'></i> <span className='hide-sm'>Logout</span>
-			</a>
-		</Fragment>
-	);
-	const guestLinks = (
-		<Fragment>
-			<Link to='/register'>Register</Link>
-			<Link to='/login'>Login</Link>
-		</Fragment>
-	);
+  const authContext = useContext(AuthContext);
+  const { isAuthenticated, logout, user } = authContext;
+  console.log(user);
+  const onLogout = () => {
+    logout();
+  };
+  const authLinks = (
+    <Fragment>
+      <li>Hello {user && user.name} </li>
+      <a onClick={onLogout} href='#!'>
+        <i className='fas fa-sign-out-alt'></i> <span className='hide-sm'>Logout</span>
+      </a>
+    </Fragment>
+  );
+  const guestLinks = (
+    <Fragment>
+      <Link to='/register'>Register</Link>
+      <Link to='/login'>Login</Link>
+    </Fragment>
+  );
 
-	return (
-		<div className='navbar bg-primary'>
-			<h1>
-				<i className={icon} /> {title}
-			</h1>
-			<ul>{isAuthenticated ? authLinks : guestLinks}</ul>
-		</div>
-	);
+  return (
+    <Fragment>
+      <div className='navbar-upper bg-dark'></div>
+      <div className='navbar bg-primary'>
+        <h1 className='text-brand'>
+          <i className={icon} /> {title}
+        </h1>
+        <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
+      </div>
+      <div className='navbar-lower bg-dark'></div>
+    </Fragment>
+  );
 };
 
 Navbar.propTypes = {
-	title: PropTypes.string.isRequired,
-	icon: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
 };
 
 Navbar.defaultProps = {
-	title: 'SplitShare',
-	icon: 'fas fa-id-card-alt',
+  title: 'SplitShare',
+  icon: 'fas fa-coins',
 };
 
 export default Navbar;
